d74bfcf282dfdec73ca7b62c757c12f1cd16cbf8
Dinesh Kumar <dineshsudhakaran2706@gmail.com>
2026-06-14 18:52:06 +0530

    updated backend server.js

--- backend/Server.js
+++ backend/Server.js
@@ -7,13 +7,17 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("ThinkHub Backend is Running! 🚀");
+});
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected!"))
-  .catch(err => console.log(err)); 
+  .catch(err => console.log(err));
 
 app.use("/api/users", require("./routes/userRoute"));
 app.use("/api/posts", require("./routes/postRoute"));
 
 app.listen(process.env.PORT, () => {
   console.log("Server running on port " + process.env.PORT);
-});    
+});
